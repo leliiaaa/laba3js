848df700a494ee262eb66c391923347326066422
leliiaaa <146171208+leliiaaa@users.noreply.github.com>
2026-03-18 20:24:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+(function () {
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
-for () {
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
   
-  if () {
-    
+  if (firstLetter === "j") {
+     byeSpeaker.speak(names[i]);
   } else {
-    
+    helloSpeaker.speak(names[i]);
   }
 }
+
+  console.log("Names ending with letter A ");
+    for (var i = 0; i < names.length; i++) {
+
+        var lastLetter = names[i].charAt(names[i].length - 1).toLowerCase();
+
+        if (lastLetter === "a") {
+            console.log(names[i]);
+        }
+    }
+})();
