57b15eaff3cac95eb65ebb19993ca09d609e1cdd
leliiaaa <146171208+leliiaaa@users.noreply.github.com>
2026-03-18 20:10:36 +0200

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,5 +1,11 @@
-var speakWord = "Good Bye";
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {};
 
 function speak(name) {
   console.log(speakWord + " " + name);
 }
+
+   window.byeSpeaker = byeSpeaker;
+
+})(window);
